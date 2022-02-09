f5fb6c019a5e21843f55d11d0853d8d9f82632b6
ac2f <mustafabasarn3@gmail.com>
2022-02-10 00:11:37 +0300

    0

--- main.py
+++ main.py
@@ -1,42 +1,76 @@
 import urllib.parse;
 import requests as rq;
 import json;
-# import keepa
-apiKey = "8ha1s159q6svbqm9n2qd1toe9uhh3g4st0nl3vrhfs667rjhd6p779gp3hsqhcf5";
-
+import keepa
+import numpy as np;
 class Keepa:
     url = None;
-    headUrl = "https://api.keepa.com/query?";
-    urlEncoded = None;
-    urlOpt = {};
-    def __init__(self, url):
+    urlDecoded = None;
+    keepa = None;
+    apiKey = None;
+    asinList = [];
+    def __init__(self, url, apiKey):
         self.url = url;
+        self.apiKey = apiKey;
+        self.keepa = keepa.Keepa(self.apiKey);
 
+    def ConcatAllKeys(self):
+        last = {};
+        data = self.UrlParamAsJson();
+        for key in data:
+            if (not key in ["selection", "key"] ): last[key] = data[key];
+        for key in data["selection"]:
+            itemData = data["selection"][key];
+            if (type(itemData) == type([0,])):
+                last[key] = itemData[0];
+                continue;
+            last[key] = itemData;
+        return last;
+    def UrlParamAsJson(self):
+        self.DecodeUrl();
+        urlOpt = {};
+        tempParams = self.url.split("?")[1].split("&");                            
+        for i in range(len(tempParams)):                                           
+            urlOpt[tempParams[i].split("=")[0]] = tempParams[i].split("=")[1];
+        urlOpt["selection"] = json.loads(urllib.parse.unquote(urlOpt["selection"]));           
+        return urlOpt;
+    def DecodeUrl(self):
+       self.urlDecoded = urllib.parse.unquote(self.url);
 
-    # def DecodeUrl(self):
-    #     self.url = urllib.parse.unquote(self.url);
-    #     tempParams = self.url.split("?")[1].split("&");
-    #     for i in range(len(tempParams)):
-    #         self.urlOpt[tempParams[i].split("=")[0]] = tempParams[i].split("=")[1];
-    #     self.urlOpt["selection"] = json.loads(self.urlOpt["selection"]);
-    # def EncodeUrl(self):
-    #     self.urlEncoded = self.headUrl+urllib.parse.urlencode(self.urlOpt)
+        # self.url = urllib.parse.unquote(self.url);                                       
+        # tempParams = self.url.split("?")[1].split("&");                                  
+        # for i in range(len(tempParams)):                                                 
+        #     self.urlOpt[tempParams[i].split("=")[0]] = tempParams[i].split("=")[1];      
+        # self.urlOpt["selection"] = json.loads(self.urlOpt["selection"]);                 
+    # def EncodeUrl(self):                                                               
+    #     self.urlEncoded = self.headUrl+urllib.parse.urlencode(self.urlOpt)             
     def GetProducts(self, max:int = 10000):
-        index = self.url.find("perPage")
-        self.url[index:index+13] = str(max);
+        index = self.url.find("perPage");
+        self.url = self.url[:index+13] + str(max) + self.url[index+16:];
+        self.asinList = rq.get(self.url).json()["asinList"];
+        return self.asinList;
+    def GetProductsData(self):
+        return self.keepa.query(np.asarray(self.asinList));
+    def GetProductData(self, productId:str):
+        return self.keepa._product_query();
+keepaCustom:Keepa = Keepa("https://api.keepa.com/query?key=8ha1s159q6svbqm9n2qd1toe9uhh3g4st0nl3vrhfs667rjhd6p779gp3hsqhcf5&domain=2&selection=%7B%2"+"2current_SALES_gte%2"+"2%3A1%2C%2"+"2current_SALES_lte%2"+"2%3A10000%2C%2"+"2rootCategory%"+"22%3A340831031%2C%2"+"2sort%"+"22%3A%5B%5B%"+"22current_SALES%"+"22%2C%"+"22asc%"+"22%5D%5D%2C%22productType%"+"22%3A%5B0%2C1%5D%2C%22perPage%"+"22%3A100%2C%22page%"+"22%3A0%7D", "8ha1s159q6svbqm9n2qd1toe9uhh3g4st0nl3vrhfs667rjhd6p779gp3hsqhcf5");
+keepaCustom.GetProducts(max = 100);
+data = keepaCustom.GetProductsData();
+print(data[98].keys()); ## kullanılabilir anahtarlar
 
-        # self.EncodeUrl();
-        
-        return self.urlEncoded;
-        # return rq.get(self.urlEncoded).json();
-keepa:Keepa = Keepa("https://api.keepa.com/query?key=8ha1s159q6svbqm9n2qd1toe9uhh3g4st0nl3vrhfs667rjhd6p779gp3hsqhcf5&domain=2&selection=%7B%2"+"2current_SALES_gte%2"+"2%3A1%2C%2"+"2current_SALES_lte%2"+"2%3A10000%2C%2"+"2rootCategory%"+"22%3A340831031%2C%2"+"2sort%"+"22%3A%5B%5B%"+"22current_SALES%"+"22%2C%"+"22asc%"+"22%5D%5D%2C%22productType%"+"22%3A%5B0%2C1%5D%2C%22perPage%"+"22%3A100%2C%22page%"+"22%3A0%7D") 
-keepa.DecodeUrl();
-print(keepa.urlOpt);
-print();
-print(keepa.GetProducts(max = 10000));
+#dict_keys(['csv', 'categories', 'imagesCSV', 'manufacturer', 'title', 'lastUpdate', 'lastPriceChange', 'rootCategory', 'productType', 'parentAsin', 
+# 'variationCSV', 'asin', 'domainId', 'type', 'hasReviews', 'trackingSince', 'brand', 'productGroup', 'partNumber', 'model', 'color', 'size', 'edition', 
+# 'format', 'packageHeight', 'packageLength', 'packageWidth', 'packageWeight', 'packageQuantity', 'isAdultProduct', 'isEligibleForTradeIn', 
+# 'isEligibleForSuperSaverShipping', 'offers', 'buyBoxSellerIdHistory', 'isRedirectASIN', 'isSNS', 'author', 'binding', 'numberOfItems', 
+# 'numberOfPages', 'publicationDate', 'releaseDate', 'languages', 'lastRatingUpdate', 'ebayListingIds', 'lastEbayUpdate', 'eanList', 'upcList', 
+# 'liveOffersOrder', 'frequentlyBoughtTogether', 'features', 'description', 'promotions', 'newPriceIsMAP', 'coupon', 'availabilityAmazon', 'listedSince', 
+# 'fbaFees', 'variations', 'itemHeight', 'itemLength', 'itemWidth', 'itemWeight', 'salesRankReference', 'salesRanks', 'salesRankReferenceHistory', 'launchpad', 
+# 'isB2B', 'stats', 'offersSuccessful', 'g', 'categoryTree', 'data'])
+
+#print(keepaCustom.ConcatAllKeys())
+# print(keepaCustom.GetProducts(max = 100));
+
 # keepa.DecodeUrl();
-print();
-print(keepa.url);
 # api = keepa.Keepa(apiKey)
 
 # params = {
